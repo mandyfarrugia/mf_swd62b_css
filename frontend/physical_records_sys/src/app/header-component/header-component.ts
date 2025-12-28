9876bf0906341dc1f7fa90ac583c905ca35d4c57
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-12-28 22:03:03 +0100

    Displaying title via property in TypeScript.

--- frontend/physical_records_sys/src/app/header-component/header-component.ts
+++ frontend/physical_records_sys/src/app/header-component/header-component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header-component',
+  selector: 'header-component',
   imports: [],
   templateUrl: './header-component.html',
   styleUrl: './header-component.css',
 })
 export class HeaderComponent {
+  private title: string = "Physical Records Registration";
 
+  public getTitle(): string {
+    return this.title;
+  }
 }
