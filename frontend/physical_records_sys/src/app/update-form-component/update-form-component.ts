2e33b334c0e4b997beb3926672ab0aa491300113
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-12-28 23:07:25 +0100

    Generate components required so far.

---
+++ frontend/physical_records_sys/src/app/update-form-component/update-form-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-form-component',
+  imports: [],
+  templateUrl: './update-form-component.html',
+  styleUrl: './update-form-component.css',
+})
+export class UpdateFormComponent {
+
+}
