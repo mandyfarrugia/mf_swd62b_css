c6ad2c668c8bd1b0b46f3b67a3b4e91edf2b8d9f
“Mandy <mandy.farrugia.c10352@mcast.edu.mt>
2025-12-31 18:06:40 +0100

    An example of null coalescing + comments about signal

--- frontend/physical_records_sys/src/components/records-list-component/records-list-component.ts
+++ frontend/physical_records_sys/src/components/records-list-component/records-list-component.ts
@@ -9,13 +9,20 @@ import { PhysicalRecordDto } from '../../dtos/physical-records-dto';
   styleUrl: './records-list-component.css',
 })
 export class RecordsListComponent implements OnInit {
-  ngOnInit(): void {
-    this.loadPhysicalRecords();
-  }
-
+  /* This signal holds an array of PhysicalRecordDto instances, initially starting off as an empty array.
+   * Signals are reactive, thus when set() is invoked within the signal,
+   * the signal will notify Angular that the state has changed (change detection is triggered)
+   * and the DOM is updated accordingly. In this case, render a table row for each physical record.
+   * In the HTML template, the value is read via physicalRecords().
+   * Simply using physicalRecords will cause the following error:
+   * TS2488: Type 'WritableSignal<PhysicalRecordDto[]>' must have a '[Symbol.iterator]()' method that returns an iterator. */
   physicalRecords = signal<PhysicalRecordDto[]>([]);
 
   constructor(private physicalRecordsService : PhysicalRecordsService) {}
+
+  ngOnInit(): void {
+    this.loadPhysicalRecords();
+  }
 
   loadPhysicalRecords() : void {
     this.physicalRecordsService.getPhysicalRecords().subscribe({
