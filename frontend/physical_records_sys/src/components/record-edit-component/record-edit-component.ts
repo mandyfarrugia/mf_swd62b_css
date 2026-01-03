e21c7f44020f9d81fcb0454b6df81054514dc2ce
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-01-03 10:43:09 +0100

    Another component to edit an existing record.

---
+++ frontend/physical_records_sys/src/components/record-edit-component/record-edit-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-record-edit-component',
+  imports: [],
+  templateUrl: './record-edit-component.html',
+  styleUrl: './record-edit-component.css',
+})
+export class RecordEditComponent {
+
+}
