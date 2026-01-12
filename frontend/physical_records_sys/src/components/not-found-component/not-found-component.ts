311b87434d1df1a928595376d795fa0698b32f48
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-01-12 21:15:10 +0100

    404 page done!

--- frontend/physical_records_sys/src/components/not-found-component/not-found-component.ts
+++ frontend/physical_records_sys/src/components/not-found-component/not-found-component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-not-found-component',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './not-found-component.html',
   styleUrl: './not-found-component.css',
 })
